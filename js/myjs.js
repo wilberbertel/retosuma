0cd7c64b5b28c25eb8079d1ebb946e8b7a27d1ab
wilberbertel <48478044+wilberbertel@users.noreply.github.com>
2019-10-01 16:28:31 -0500

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,4 @@
+function aleatorio(){
+document.getElementById("numero1").innerHTML = Math.floor(Math.random() * (10 - 0)) + 0;
+document.getElementById("numero2").innerHTML = Math.floor(Math.random() * (10 - 0)) + 0;
+}
